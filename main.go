9f3c2b29d99e863501d03a50619e1f6eae4ace6a
Oxore <oxore@protonmail.com>
2022-01-01 02:12:55 +0300

    Impl string parsing

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ type TokenType int
 
 const (
 	TokNumber TokenType = iota
-	TokBuiltinDefun
+	TokBuiltinDefine
 	TokBuiltinLambda
-	TokBuiltinSetq
+	TokBuiltinQuote
 	TokIdentifier
 	TokString
 	TokLparen
@@ -32,12 +32,12 @@ func (token Token) String() string {
 	switch token.Type {
 	case TokNumber:
 		return "TokNumber"
-	case TokBuiltinDefun:
-		return "TokBuiltinDefun"
+	case TokBuiltinDefine:
+		return "TokBuiltinDefine"
 	case TokBuiltinLambda:
 		return "TokBuiltinLambda"
-	case TokBuiltinSetq:
-		return "TokBuiltinSetq"
+	case TokBuiltinQuote:
+		return "TokBuiltinQuote"
 	case TokIdentifier:
 		return "TokIdentifier"
 	case TokString:
@@ -62,6 +62,8 @@ const (
 	LexIdle State = iota
 	LexNumber
 	LexIdentifier
+	LexString
+	LexStringEscaped
 )
 
 type TokensFormatter struct {
@@ -117,6 +119,17 @@ func (self *Lex) BeginIdentifier() {
 	self.State = LexIdentifier
 }
 
+func (self *Lex) BeginString() []Token {
+	var newTokens []Token
+	if (self.State != LexIdle) {
+		newTokens = append(newTokens, self.Tokens[len(self.Tokens)-1])
+	}
+	newToken := Token{self.Source.Len(), 1, TokString}
+	self.Tokens = append(self.Tokens, newToken)
+	self.State = LexString
+	return newTokens
+}
+
 func IsNumeric(c byte) bool {
 	return c >= '0' && c <= '9'
 }
@@ -125,57 +138,126 @@ func IsAlphabetic(c byte) bool {
 	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '_')
 }
 
+func IsCharacter(c byte) bool {
+	return c == 0x09 || (c >= ' ' && c <= '~')
+}
+
 func IsAlphaNumeric(c byte) bool {
 	return IsNumeric(c) || IsAlphabetic(c)
 }
 
-func (self *Lex) Consume(c byte) []Token {
-	var newTokens []Token
+func IsSingleCharToken(c byte) bool {
+	return c == '(' || c == ')' || c == '.'
+}
+
+func TokenFromByte(c byte) TokenType {
 	if c == '(' {
-		newTokens = append(newTokens, self.AddToken(TokLparen)...)
+		return TokLparen
 	} else if c == ')' {
-		newTokens = append(newTokens, self.AddToken(TokRparen)...)
+		return TokRparen
 	} else if c == '.' {
-		newTokens = append(newTokens, self.AddToken(TokDot)...)
-	} else if c == ' ' || c == 0x0A || c == 0x0D {
-		if self.State != LexIdle {
+		return TokDot
+	}
+	panic(fmt.Sprintf("Byte %v cannot be converted to token", c))
+	return TokDot
+}
+
+func (self *Lex) Consume(c byte) []Token {
+	// TODO do not use the variable, always return instead of `append`
+	var newTokens []Token
+	switch self.State {
+	case LexIdle:
+		if IsSingleCharToken(c) {
+			newTokens = append(newTokens, self.AddToken(TokenFromByte(c))...)
+		} else if c == ' ' || c == 0x0A || c == 0x0D {
+			if self.State != LexIdle {
+				self.State = LexIdle
+				newTokens = append(newTokens, self.Tokens[len(self.Tokens)-1])
+			}
+		} else if IsNumeric(c) {
+			self.BeginNumber()
+		} else if IsAlphaNumeric(c) {
+			self.BeginIdentifier()
+		} else if c == '"' {
+			newTokens = append(newTokens, self.BeginString()...)
+		} else {
+			// TODO raise error
+			panic("unexpected byte")
+		}
+	case LexNumber:
+		if IsSingleCharToken(c) {
+			newTokens = append(newTokens, self.AddToken(TokenFromByte(c))...)
+		} else if c == '"' {
+			self.BeginString()
+		} else if c == ' ' || c == 0x0A || c == 0x0D {
+			if self.State != LexIdle {
+				self.State = LexIdle
+				newTokens = append(newTokens, self.BeginString()...)
+			}
+		} else if IsNumeric(c) {
+			token := self.Tokens[len(self.Tokens)-1]
+			token.Length += 1
+			self.Tokens[len(self.Tokens)-1] = token
+		} else if IsAlphaNumeric(c) {
+			token := self.Tokens[len(self.Tokens)-1]
+			token.Length += 1
+			token.Type = TokIdentifier
+			self.Tokens[len(self.Tokens)-1] = token
+		} else {
+			// TODO raise error
+			panic("unexpected byte")
+		}
+	case LexIdentifier:
+		if IsSingleCharToken(c) {
+			newTokens = append(newTokens, self.AddToken(TokenFromByte(c))...)
+		} else if c == '"' {
+			self.BeginString()
+		} else if c == ' ' || c == 0x0A || c == 0x0D {
+			if self.State != LexIdle {
+				self.State = LexIdle
+				newTokens = append(newTokens, self.Tokens[len(self.Tokens)-1])
+			}
+		} else if IsAlphaNumeric(c) {
+			token := self.Tokens[len(self.Tokens)-1]
+			token.Length += 1
+			self.Tokens[len(self.Tokens)-1] = token
+		} else {
+			// TODO raise error
+			panic("unexpected byte")
+		}
+	case LexStringEscaped:
+		if c == '\\' {
+			token := self.Tokens[len(self.Tokens)-1]
+			token.Length += 1
+			self.Tokens[len(self.Tokens)-1] = token
+		} else if IsCharacter(c) {
+			token := self.Tokens[len(self.Tokens)-1]
+			token.Length += 1
+			self.Tokens[len(self.Tokens)-1] = token
+		} else {
+			// TODO raise error
+			panic("unexpected byte")
+		}
+		self.State = LexString
+	case LexString:
+		if c == '"' {
+			token := self.Tokens[len(self.Tokens)-1]
+			token.Length += 1
+			self.Tokens[len(self.Tokens)-1] = token
 			self.State = LexIdle
 			newTokens = append(newTokens, self.Tokens[len(self.Tokens)-1])
-		}
-	} else {
-		switch self.State {
-		case LexIdle:
-			if IsNumeric(c) {
-				self.BeginNumber()
-			} else if IsAlphaNumeric(c) {
-				self.BeginIdentifier()
-			} else {
-				// TODO raise error
-				panic("unexpected byte")
-			}
-		case LexNumber:
-			if IsNumeric(c) {
-				token := self.Tokens[len(self.Tokens)-1]
-				token.Length += 1
-				self.Tokens[len(self.Tokens)-1] = token
-			} else if IsAlphaNumeric(c) {
-				token := self.Tokens[len(self.Tokens)-1]
-				token.Length += 1
-				token.Type = TokIdentifier
-				self.Tokens[len(self.Tokens)-1] = token
-			} else {
-				// TODO raise error
-				panic("unexpected byte")
-			}
-		case LexIdentifier:
-			if IsAlphaNumeric(c) {
-				token := self.Tokens[len(self.Tokens)-1]
-				token.Length += 1
-				self.Tokens[len(self.Tokens)-1] = token
-			} else {
-				// TODO raise error
-				panic("unexpected byte")
-			}
+		} else if c == '\\' {
+			token := self.Tokens[len(self.Tokens)-1]
+			token.Length += 1
+			self.Tokens[len(self.Tokens)-1] = token
+			self.State = LexStringEscaped
+		} else if IsCharacter(c) {
+			token := self.Tokens[len(self.Tokens)-1]
+			token.Length += 1
+			self.Tokens[len(self.Tokens)-1] = token
+		} else {
+			// TODO raise error
+			panic("unexpected byte")
 		}
 	}
 	self.Source.WriteByte(c)
